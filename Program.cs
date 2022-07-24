2fa06061a5603e99696d153a3f00ef0b848da94e
YuriyP <solovej-1991@mail.ru>
2022-07-24 18:48:51 +0600

    решение задачи 60

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@
 // 1 2 4 7
 // 2 3 5 9
 // 2 4 4 8
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 
 // int [,] generateRandomArray(int z, int x, int minNumber, int maxNumber) // создание функции для создания двухмерного массива
 // {
@@ -72,7 +73,7 @@
 // sortingArray(array);
 // printArray(array);
 
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 // Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 // Например, задан массив:
 // 1 4 7 2
@@ -80,6 +81,7 @@
 // 8 4 2 4
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 
 // int [,] generateRandomArray1(int z, int x, int minNumber, int maxNumber) // создание функции для создания двухмерного массива
 // {
@@ -148,7 +150,7 @@
 // Console.Write("Cтрока с наименьшей суммой элементов -> ");
 // Console.Write (getRowArray(array1));
 
-
+//-------------------------------------------------------------------------------------------------------------------------------------------------
 // Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
 // Например, заданы 2 массива:
 // 1 4 7 2              1 5 8 5
@@ -160,96 +162,156 @@
             // 20 81 8 6
             // 56 8 4 24
             // 10 6 24 49
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+// int [,] generateRandomArray2(int z, int x, int minNumber, int maxNumber) // создание функции для создания двухмерного массива
+// {
+//     int [,] rnArray = new int [z, x];
+//     for (int i = 0; i < z; i++)
+//     {
+//         for (int j = 0; j < x; j++)
+//         {
+//             rnArray[i, j] = new Random().Next(minNumber, maxNumber+1); //заполнение массива случайными числами
+//         }
+//     }
+//     return rnArray;
+// }
+
+// void printArray2(int [,] prArray) // создание метода для вывода массива на консоль
+// {
+//     for (int i = 0; i < prArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < prArray.GetLength(1); j++)
+//         {
+//             Console.Write($"{prArray[i, j]} ");
+//         }
+//     Console.WriteLine(" ");
+//     }
+// }
+
+// int [,] compositionArray(int [,] comArray, int [,] comArray1)
+// {
+//     int [,] multArray = new int [comArray.GetLength(0), comArray.GetLength(1)];
+//     for (int i = 0; i < comArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < comArray1.GetLength(1); j++)
+//         {
+//             multArray[i,j] = (comArray[i, j]*comArray1[j, i]);                
+//         }
+//     }
+//     return multArray;
+// }
 
 
-int [,] generateRandomArray2(int z, int x, int minNumber, int maxNumber) // создание функции для создания двухмерного массива
+// Console.Clear();
+// Console.Write("Введите количество строк первого массива: ");
+// int rows2=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов первого массива: ");
+// int columns2=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное число первого массива: ");
+// int min2=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное число первого массива: ");
+// int max2=Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите количество строк второго массива: ");
+// int rows3=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите количество столбцов второго массива: ");
+// int columns3=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите минимальное число второго массива: ");
+// int min3=Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите максимальное число второго массива: ");
+// int max3=Convert.ToInt32(Console.ReadLine());
+
+
+// int [,] array2 = generateRandomArray2 (rows2, columns2, min2, max2);
+// int [,] array3 = generateRandomArray2 (rows3, columns3, min3, max3);
+// int [,]  multiplicationArray = new int [array2.GetLength(0), array2.GetLength(1)];
+
+// if (array2.GetLength(0)!=array3.GetLength(1))
+//     {
+//         Console.WriteLine("Введеные массивы нельзя перемножить, так как строки или столбцы не равны");
+//         return;
+//     }
+//     else
+//     {
+
+//         multiplicationArray = compositionArray(array2, array3);
+//     }
+    
+// printArray2(array2);
+// Console.WriteLine(" ");
+// printArray2(array3);
+
+// Console.WriteLine("матричное произведение двух массивов равно:");
+// printArray2(multiplicationArray);
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// массив размером 2 x 2 x 2
+// 12(0,0,0) 22(0,0,1)
+// 45(1,0,0) 53(1,0,1)
+//-------------------------------------------------------------------------------------------------------------------------------------------------
+
+int [,,] generateRandomArray3(int z, int x, int c, int minNumber, int maxNumber) // создание функции для создания трехмерного массива с неповторяющими двухзначными числами
 {
-    int [,] rnArray = new int [z, x];
+    int [,,] rnArray = new int [z, x, c];
     for (int i = 0; i < z; i++)
     {
         for (int j = 0; j < x; j++)
         {
-            rnArray[i, j] = new Random().Next(minNumber, maxNumber+1); //заполнение массива случайными числами
+            for (int k = 0; k < c; k++)
+            {
+                int number = new Random().Next(minNumber, maxNumber+1); 
+                if (findElement(rnArray, number)) continue;
+                rnArray[i, j, k] = number;                        
+
+            }
         }
     }
     return rnArray;
 }
 
-void printArray2(int [,] prArray) // создание метода для вывода массива на консоль
+bool findElement (int [,,] array, int element)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                if (array[i,j,k] == element) return true;
+            }
+        }
+    }
+    return false;
+}
+
+void printArray2(int [,,] prArray) // создание метода для вывода массива на консоль
 {
     for (int i = 0; i < prArray.GetLength(0); i++)
     {
         for (int j = 0; j < prArray.GetLength(1); j++)
         {
-            Console.Write($"{prArray[i, j]} ");
-        }
-    Console.WriteLine(" ");
-    }
-}
-
-int [,] compositionArray(int [,] comArray, int [,] comArray1)
-{
-    int [,] multArray = new int [comArray.GetLength(0), comArray.GetLength(1)];
-    for (int i = 0; i < comArray.GetLength(0); i++)
-    {
-        for (int j = 0; j < comArray1.GetLength(1); j++)
-        {
-            multArray[i,j] = (comArray[i, j]*comArray1[j, i]);                
+            for (int k = 0; k < prArray.GetLength(2); k++)
+            {
+                Console.Write($" {prArray[i, j, k]} ({i}, {j}, {k})");
+            }
+            Console.WriteLine(" ");
         }
     }
-    return multArray;
 }
-
 
 Console.Clear();
-Console.Write("Введите количество строк первого массива: ");
-int rows2=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов первого массива: ");
-int columns2=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите минимальное число первого массива: ");
-int min2=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное число первого массива: ");
-int max2=Convert.ToInt32(Console.ReadLine());
-
-Console.Write("Введите количество строк второго массива: ");
-int rows3=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите количество столбцов второго массива: ");
-int columns3=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите минимальное число второго массива: ");
-int min3=Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите максимальное число второго массива: ");
-int max3=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество строк массива: ");
+int rows4=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество столбцов массива: ");
+int columns4=Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите количество высота массива: ");
+int height4=Convert.ToInt32(Console.ReadLine());
 
 
-int [,] array2 = generateRandomArray2 (rows2, columns2, min2, max2);
-int [,] array3 = generateRandomArray2 (rows3, columns3, min3, max3);
-int [,]  multiplicationArray = new int [array2.GetLength(0), array2.GetLength(1)];
-
-if (array2.GetLength(0)!=array3.GetLength(1))
-    {
-        Console.WriteLine("Введеные массивы нельзя перемножить, так как строки или столбцы не равны");
-        return;
-    }
-    else
-    {
-
-        multiplicationArray = compositionArray(array2, array3);
-    }
-    
-printArray2(array2);
-Console.WriteLine(" ");
-printArray2(array3);
-
-Console.WriteLine("матричное произведение двух массивов равно:");
-printArray2(multiplicationArray);
-
-
-// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
-// массив размером 2 x 2 x 2
-// 12(0,0,0) 22(0,0,1)
-// 45(1,0,0) 53(1,0,1)
-
-
+int [,,] array4 = generateRandomArray3 (rows4, columns4, height4, 10, 99);
+printArray2(array4);
 
 
 // Задача 62. Заполните спирально массив 4 на 4.
